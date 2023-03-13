609b02897d0155a724ad9ec3166e690faf6cc499
MLibralato91 <michelelibralato911@gmail.com>
2023-03-13 15:31:51 +0100

    new project, add class email array

---
+++ js/script.js
@@ -0,0 +1,32 @@
+/*
+Esercitazione
+--------------------------------------------------------
+
+Mail
+
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+
+----------------------------------------------------------
+Gioco dei dadi
+
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+*/ 
+
+let classEmail = [
+    'lisa@email.com',
+    'marco@email.com',
+    'sara@email.com',
+    'chiara@email.com',
+    'samuele@email.com',
+    'simone@email.com',
+    'michele@email.com',
+    'giorgia@email.com',
+    'simona@email.com',
+    'emanuele@email.com',
+    'mirko@email.com',
+]
+console.log(classEmail);
